899487848462e7612d0d6b417320c8ad308e9c03
alexeyfominyh <alexeyfominyh5@gmail.com>
2022-11-18 16:49:24 +0600

    chore

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log("Hello world")
+import { Telegraf } from "telegraf";
+
+const bot = new Telegraf("5856963941:AAEMeid97yMnoVSAfw8tK2nuSwGRtgZk6xo");
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hello!!!!!'));
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
